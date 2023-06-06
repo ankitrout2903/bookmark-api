516575f9f4319f64fd9eddad18b5ee4267f22172
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-06-06 19:26:14 +0530

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post} from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto : AuthDto) {
+    console.log({dto});
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+    constructor(private prismaService: PrismaService) {
+    }
     signin() {
         return { msg: 'I am signin' }
     }
-
-
     signup() {
         return { msg: 'I am signup' }
     }
 }
-const service = new AuthService();
