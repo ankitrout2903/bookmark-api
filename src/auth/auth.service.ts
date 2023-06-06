38dea0b61c1844691e3b6c9331690951d4159bbe
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-06-06 21:36:45 +0530

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
     constructor(private prismaService: PrismaService) {
     }
+    signup(dto : AuthDto) {
+        return { msg: 'I am signup' }
+    }
     signin() {
         return { msg: 'I am signin' }
     }
-    signup() {
-        return { msg: 'I am signup' }
-    }
-}
+   
+} 
