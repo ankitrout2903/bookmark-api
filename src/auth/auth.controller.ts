e8ac4f74db3960b8cb93f025a98c3e965adefe2c
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-06-10 01:58:23 +0530

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
@@ -9,10 +8,10 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    // console.log({ dto });
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
+ 
   @Get('me')
-  getMe(@Req() req: Request ) {
-    return req.user;
+  getMe(@GetUser() user: User ,) {
+    return user;
   }
-}
 
+  @Patch()
+  editUser() {}
+}
